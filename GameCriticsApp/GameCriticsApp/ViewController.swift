12cba93c978e743f31d89d66c5cfb952469de34f
yemretekin <emretekin08@gmail.com>
2022-12-06 18:48:33 +0300

    xcode project created

---
+++ GameCriticsApp/GameCriticsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameCriticsApp
+//
+//  Created by Emre Tekin on 6.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
